17607fc081e180c2f173cb8a8e7790a996482475
leo <1659897544@qq.com>
2020-06-12 00:07:56 +0800

    addtwonumbers

---
+++ src/addtwonumbers/Solution.java
@@ -0,0 +1,28 @@
+package addtwonumbers;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	
+	int x,y = 0;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {   
+    	
+    	if(l1 == null && l2 == null) {//节点为空 直接返回null
+    		if(y!=0)
+    			return new ListNode(y);
+    		return null;
+    	}
+
+    	x = ((l1==null?0:l1.val) + (l2==null?0:l2.val) + y);//计算节点之和
+    	ListNode head = new ListNode(x%10);//将各位置入新的数组
+    	y = x/10;//取进位
+    	head.next= addTwoNumbers(l1.next, l2.next);
+    	return head;
+
+    }
+}
