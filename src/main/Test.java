e47d5b5866c00d7204c7bce03701ef6ffaade72a
leo <18890395849@163.com>
2020-06-17 12:43:49 +0800

    字符串转换整数 (atoi)

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,10 +1,21 @@
 package main;
 
+import isPalindrome.Solution;
+
+//import myAtoi.*;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Integer.MAX_VALUE);
+//		System.out.println(Long.MAX_VALUE);
+//		long l = 9223372036854775808;
+//		String str = "9223372036854775808";
+//    	System.out.println(str.matches("\\s*\\-?\\+?[0-9]+.*"));//表示匹配以“ ”/字母开始的任意字符串
+//		System.out.println(new Solution().myAtoi("-256123 hello")); 
+		
+		System.out.println(new Solution().isPalindrome(-121));
+//		System.out.println(String.valueOf(-2));
 	}
 
 }
--- src/myAtoi/Solution.java
+++ src/myAtoi/Solution.java
@@ -1,14 +1,31 @@
 package myAtoi;
 class Solution {
     public int myAtoi(String str) {
-        char[] arrays = new char[str.length()];
-        if(str.matches("\\b+")) {
-        	return 0;
+    	if(!str.matches("\\s*\\-?\\+?[0-9]+.*")) {
+    		return 0;
+    	}
+        char[] arrays = str.trim().toCharArray();
+        long sum = 0;
+        for(int i=0;i<arrays.length;i++) {
+        	if(arrays[i] >='0' && arrays[i] <='9') {
+        		sum *=10;
+        		if(str.matches("\\s*\\-.*")) {
+        			sum -= arrays[i] - '0';
+        		}
+        		else {
+        			sum += arrays[i] - '0';
+        		}
+        		if(sum>=Integer.MAX_VALUE) {
+        			return Integer.MAX_VALUE;
+        		}
+        		if(sum<=Integer.MIN_VALUE) {
+        			return Integer.MIN_VALUE;
+        		}
+        		
+        	}else if(i>0){
+        		break;
+        	}
         }
-        else if(){
-        	
-        }
-        
-        return 0;
+        return (int)sum;
     }
 }
